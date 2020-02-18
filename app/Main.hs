0b502ddf912dbd36ff36eb1806f0939d6dc8f77b
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2020-02-18 20:03:01 +0700

    Implement a RSS parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,27 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import Network.HTTP
+import Text.XML.Light (parseXML)
+
+import Control.Monad
+import Data.Maybe
+import System.Exit
+import System.IO
+
+import RSS
+
+
+feedUrl = "feed://fit.nsu.ru/component/ninjarsssyndicator/?feed_id=1&format=raw"
+
+main = do
+    -- Получаем данные RSS-ленты
+    feed <- simpleHTTP (getRequest feedUrl) >>= getResponseBody
+
+    -- Находим корневой тег "rss", в противном случае завершаем программу
+    let root = findRoot $ parseXML feed
+    when (isNothing root) $ do putStrLn "Root RSS node not found!" ; exitFailure
+
+    let channels = parseChannels (fromJust root)
+    
+    hSetEncoding stdout latin1
+    putStrLn $ show (head channels)
---
+++ app/RSS.hs
@@ -0,0 +1,55 @@
+module RSS
+( findRoot
+, parseChannels
+) where
+
+import Data.Maybe (fromJust)
+import Data.List (find)
+import Text.XML.Light
+
+
+data Item = Item {
+    itTitle :: String,
+    itLink :: String
+}
+
+data Channel = Channel {
+    chTitle :: String,
+    chDescription :: String,
+    chItems :: [Item]
+}
+
+instance Show Item where
+    show (Item title link) = title ++ "\n" ++ link ++ "\n"
+
+instance Show Channel where
+    show (Channel title description items) =
+        fullTitle ++ "\n" ++ content where
+            fullTitle = title ++ " - " ++ description
+            content = unlines $ map show items
+
+
+findRoot :: [Content] -> Maybe Element
+findRoot = findRoot' . onlyElems
+    where findRoot' = find $ (== QName "rss" Nothing Nothing) . elName
+
+
+prop :: Element -> String -> String
+prop node name = strContent . fromJust $ findChild (QName name Nothing Nothing) node
+
+
+parseItem :: Element -> Item
+parseItem node = Item { itTitle = title, itLink = link }
+    where title = prop node "title"
+          link = prop node "link"
+
+
+parseChannel :: Element -> Channel
+parseChannel node = Channel { chTitle = title, chDescription = desc, chItems = items }
+    where title = prop node "title"
+          desc = prop node "description"
+          items = map parseItem $ findChildren (QName "item" Nothing Nothing) node
+
+
+parseChannels :: Element -> [Channel]
+parseChannels root = map parseChannel $ findChildren (QName "channel" Nothing Nothing) root
