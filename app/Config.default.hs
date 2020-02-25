d7b3a0e7ec5c2e479c0dad6e5f7258df139903ef
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2020-02-25 10:59:43 +0700

    Add a config file

---
+++ app/Config.default.hs
@@ -0,0 +1,10 @@
+module Config where
+
+botAuthToken :: String
+botAuthToken = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"
+
+chatId :: String
+chatId = "@channelusername"
+
+pollingDelay :: Int
+pollingDelay = 30
